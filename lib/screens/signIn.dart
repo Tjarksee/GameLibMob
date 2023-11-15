9fa9fe4bf43792bcf0172079729fe161bfa21a7c
tjark <Tjark.see@gmail.com>
2023-11-15 01:20:27 +0100

    error messages are shown

--- lib/screens/signIn.dart
+++ lib/screens/signIn.dart
@@ -12,18 +12,22 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  bool showError = false;
+  String errorMessage = '';
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
         body: Container(
             decoration: const BoxDecoration(
                 gradient: LinearGradient(colors: [
-                  Color.fromARGB(249, 50, 48, 50),
-                  Color.fromARGB(249, 108, 106, 108),
-                  Color.fromARGB(249, 50, 48, 50)
-                ], begin: Alignment.bottomRight, end: Alignment.topLeft)),
+              Color.fromARGB(249, 50, 48, 50),
+              Color.fromARGB(249, 108, 106, 108),
+              Color.fromARGB(249, 50, 48, 50)
+            ], begin: Alignment.bottomRight, end: Alignment.topLeft)),
             child: Padding(
                 padding: EdgeInsets.fromLTRB(
                     20, MediaQuery.of(context).size.height * 0.2, 20, 0),
@@ -33,24 +37,45 @@ class _SignInScreenState extends State<SignInScreen> {
                   const SizedBox(
                     height: 20,
                   ),
-                  reusableTextField("Enter Password", Icons.lock_outline, true,
-                      _passwordTextController),
+                  reusableTextField("Enterdsa Password", Icons.lock_outline,
+                      true, _passwordTextController),
                   const SizedBox(
                     height: 20,
                   ),
-                  firebaseUIButton(context, "Login", () {
-                    FirebaseAuth.instance
-                        .signInWithEmailAndPassword(
-                        email: _emailTextController.text,
-                        password: _passwordTextController.text)
-                        .then((value) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomeScreen()));
-                    }).onError((error, stackTrace) {
-                      print("Error ${error.toString()}");
-                    });
+                  TextFormField(
+                    autocorrect: false,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return errorMessage;
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    showError ? errorMessage : '',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  firebaseUIButton(context, "Login", () async {
+                    try {
+                      await FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: _emailTextController.text,
+                              password: _passwordTextController.text)
+                          .then((value) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()));
+                      });
+                    } on FirebaseAuthException catch (e) {
+                      errorMessage = e.code;
+                      setState(() {
+                        showError = true;
+                      });
+                    }
                   }),
                   signUpOption()
                 ]))));
