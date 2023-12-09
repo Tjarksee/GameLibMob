f83e241ac7c411667eb3d044cc568a544a8eccc2
Smaazher <Eikehhn@gmail.com>
2023-12-09 15:59:17 +0100

    Implementierung des firebase_traffic und der push und pull funktionen

--- lib/firebase_traffic.dart
+++ lib/firebase_traffic.dart
@@ -1,23 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+final firestoreInstance = FirebaseFirestore.instance;
 
 void pullFirebase(){
 
 }
 
-void pushFirebase(){
-
-}
-
-firestoreInstance.collection("User").add({
-"userName": "john",
-"email": "example@example.com",
-"password": "test"
-}).then((value) {
+void pushFirebase() {
+  firestoreInstance.collection('test').add({
+    'userName': "test",
+    'stadt' : "kiel",
+  }).then((value){
     print(value.id);
-    firestoreInstance
-    .collection("User")
-    .doc(value.id)
-    .collection("gameList")
-  for(int i = 0,i <  ,i++){
-    .add({gameList[i]});
+  });
 }
-});
+
